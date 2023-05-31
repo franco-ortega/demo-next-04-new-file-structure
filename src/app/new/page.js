9a18488bf34dae570d9ff21c5d04dc6f2dfadcb3
Franco Ortega <francopdx@gmail.com>
2023-05-31 16:57:59 -0700

    comment out createTodo

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,20 +1,22 @@
 import Link from 'next/link';
 
-async function createTodo(data) {
-  'use server';
+// async function createTodo(data) {
+//   ('use server');
 
-  console.log('hi');
-}
+//   console.log('hi', data.title);
+// }
 
 function Page() {
   return (
     <>
       <h2>Create a new Todo</h2>
-      <form>
+      <form
+      // action={createTodo((e) => e.target.value)}
+      >
         <input type="text" name="title" />
         <div>
           <Link href="..">Cancel</Link>
-          <button>Create</button>
+          <button type="submit">Create</button>
         </div>
       </form>
     </>
